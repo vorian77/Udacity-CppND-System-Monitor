5789ddad7321f65316f65cf613e11ff64994891f
Phyllip Hall <phall776@gmail.cominit>
2021-01-16 10:47:32 -0500

    set process sort to Ram

--- src/process.cpp
+++ src/process.cpp
@@ -44,6 +44,5 @@ string Process::Command() { return Command_; }
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const { 
-    return this->CpuUtilization_ > a.CpuUtilization_;
-    //return this->RamUtilization_ > a.RamUtilization_; 
+    return this->RamUtilization_ > a.RamUtilization_; 
 }
